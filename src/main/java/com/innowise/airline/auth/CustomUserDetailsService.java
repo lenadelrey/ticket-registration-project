0004c6d6a7d2241437bc5b3e333cdbaa844a9cc5
lenadelrey <lenka-simonova@mail.ru>
2022-04-11 23:47:53 +0300

    fixed service

--- src/main/java/com/innowise/airline/auth/CustomUserDetailsService.java
+++ src/main/java/com/innowise/airline/auth/CustomUserDetailsService.java
@@ -2,15 +2,17 @@ package com.innowise.airline.auth;
 
 import com.innowise.airline.model.User;
 import com.innowise.airline.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
