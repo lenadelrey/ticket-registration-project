1851bbb9636a7c424c513b1f8241401e55873312
lenadelrey <lenka-simonova@mail.ru>
2022-04-11 13:04:25 +0300

    added new methods

--- src/main/java/com/innowise/airline/repository/AirlineRepository.java
+++ src/main/java/com/innowise/airline/repository/AirlineRepository.java
@@ -1,9 +1,14 @@
 package com.innowise.airline.repository;
 
 import com.innowise.airline.model.Airline;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AirlineRepository extends JpaRepository<Airline, Long> {
+
+    @Override
+    Page<Airline> findAll(Pageable pageable);
 }
--- src/main/java/com/innowise/airline/repository/FlightRepository.java
+++ src/main/java/com/innowise/airline/repository/FlightRepository.java
@@ -1,9 +1,15 @@
 package com.innowise.airline.repository;
 
 import com.innowise.airline.model.Flight;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface FlightRepository extends JpaRepository<Flight, Long> {
+
+    Page<Flight> findAll(Pageable pageable);
+
 }
