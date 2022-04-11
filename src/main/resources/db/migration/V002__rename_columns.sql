a873e23391c9743ed9db747991e555e081aa250b
lenadelrey <lenka-simonova@mail.ru>
2022-04-11 15:41:07 +0300

    renamed columns

---
+++ src/main/resources/db/migration/V002__rename_columns.sql
@@ -0,0 +1,7 @@
+alter table "user" rename column user_id to id;
+
+alter table ticket rename column ticket_id to id;
+
+alter table flight rename column flight_id to id;
+
+alter table airline rename column airline_id to id;
